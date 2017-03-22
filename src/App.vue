19412da6f62302547d8a3ee7ab5bbd4ab38ab70f
Mozartted <mozart.osita@gmail.com>
2017-03-22 22:45:57 +0100

    Changes here

--- src/App.vue
+++ src/App.vue
@@ -14,12 +14,18 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style>
+  *{
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
+  html{
+    font-family: sans-serif;
+  }
+  body{
+    background: #eee;
+    padding: 0 16px;
+  }
+</style>
 </style>
